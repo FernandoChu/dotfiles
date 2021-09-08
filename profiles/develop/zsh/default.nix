8a3888b12c82f36fbe0d56c3470086dcbc9e4fe0
fernando <fernandochu97@gmail.com>
2021-09-08 11:30:37 -0500

    zsh stuff

--- profiles/develop/zsh/default.nix
+++ profiles/develop/zsh/default.nix
@@ -32,5 +32,74 @@ in
       "autocontinue"
       "promptsubst"
     ];
+
+    interactiveShellInit =
+      let
+        zshrc = fileContents ./zshrc;
+
+        sources = with pkgs; [
+          # ./cdr.zsh
+          "${skim}/share/skim/completion.zsh"
+          "${oh-my-zsh}/share/oh-my-zsh/plugins/sudo/sudo.plugin.zsh"
+          "${oh-my-zsh}/share/oh-my-zsh/plugins/extract/extract.plugin.zsh"
+          "${zsh-you-should-use}/share/zsh/plugins/you-should-use/you-should-use.plugin.zsh"
+          "${zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+          "${zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh"
+
+        ];
+
+        source = map (source: "source ${source}") sources;
+
+        functions = pkgs.stdenv.mkDerivation {
+          name = "zsh-functions";
+          src = ./functions;
+
+          ripgrep = "${pkgs.ripgrep}";
+          man = "${pkgs.man}";
+          exa = "${pkgs.exa}";
+
+          installPhase =
+            let basename = "\${file##*/}";
+            in
+            ''
+              mkdir $out
+
+              for file in $src/*; do
+                substituteAll $file $out/${basename}
+                chmod 755 $out/${basename}
+              done
+            '';
+        };
+
+        plugins = concatStringsSep "\n" source;
+
+        localCompletions = toString ./completions;
+
+        bashCompletion = ''
+          complete -o nospace -C ${pkgs.nomad}/bin/nomad nomad
+          complete -o nospace -C ${pkgs.consul}/bin/consul consul
+          complete -o nospace -C ${pkgs.vault-bin}/bin/vault vault
+          complete -o nospace -C ${pkgs.terraform}/bin/terraform terraform
+          complete -o nospace -C ${pkgs.awscli}/bin/aws_completer aws
+        '';
+
+      in
+      ''
+        ${plugins}
+
+        fpath+=( ${functions} ${localCompletions} )
+        autoload -Uz ${functions}/*(:t)
+
+        ${zshrc}
+
+        ${bashCompletion}
+
+        eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
+        eval $(${pkgs.gitAndTools.hub}/bin/hub alias -s)
+        source ${pkgs.skim}/share/skim/key-bindings.zsh
+
+        # needs to remain at bottom so as not to be overwritten
+        bindkey jj vi-cmd-mode
+      '';
   };
 }
