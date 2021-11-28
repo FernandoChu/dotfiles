5b6f6a3a5abe47877946b9b4b6a85c8a357516ac
fernando <fernandochu97@gmail.com>
2021-11-28 15:20:16 -0500

    tex

--- profiles/develop/neovim/default.nix
+++ profiles/develop/neovim/default.nix
@@ -4,22 +4,29 @@
     vimAlias = true;
     viAlias = true;
     defaultEditor = true;
-    # configure = {
-    #   customRC = ''
-    #     set shiftwidth=2
-    #     set softtabstop=2
-    #     set expandtab
-    #     set background=dark
-    #     let g:deoplete#enable_at_startup = 1
-    #   '';
-    #   packages.myVimPackage = with super.vimPlugins; {
-    #     start = [
-    #       vim-startify  airline             sensible
-    #       polyglot      ale                 fugitive
-    #       deoplete-nvim
-    #     ];
-    #     opt = [ ];
-    #   };
-    # };
+    configure = {
+      customRC = ''
+        set shiftwidth=2
+        set softtabstop=2
+        set expandtab
+        set background=dark
+        let g:deoplete#enable_at_startup = 1
+        ${(fileContents ./coc.vim)}
+      '';
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [
+          vim-startify
+          airline
+          sensible
+          polyglot
+          ale
+          fugitive
+          deoplete-nvim
+          coc-vimtex
+          coc-nvim
+        ];
+        opt = [ ];
+      };
+    };
   };
 }
